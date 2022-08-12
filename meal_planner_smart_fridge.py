66c39f48b896ffdeea92de7743caaf4b54d0c9ca
pedroarchilha <pedroarchilha@yahoo.com.br>
2022-08-12 19:11:55 -0300

    Smart fridge. Intended to use an API to send the items to a notepad app (i.e Evernote)

---
+++ meal_planner_smart_fridge.py
@@ -0,0 +1,114 @@
+pantry = {
+    "chicken": 500,
+    "lemon": 2,
+    "cumin": 24,
+    "paprika": 18,
+    "chilli powder": 7,
+    "yogurt": 300,
+    "oil": 450,
+    "onion": 5,
+    "garlic": 9,
+    "ginger": 2,
+    "tomato puree": 125,
+    "almonds": 75,
+    "rice": 500,
+    "coriander": 20,
+    "lime": 3,
+    "pepper": 8,
+    "egg": 6,
+    "pizza": 2,
+    "spam": 1,
+}
+
+recipes = {
+    "Butter chicken": {
+        "chicken": 750,
+        "lemon": 1,
+        "cumin": 1,
+        "paprika": 1,
+        "chilli powder": 2,
+        "yogurt": 250,
+        "oil": 50,
+        "onion": 1,
+        "garlic": 2,
+        "ginger": 3,
+        "tomato puree": 240,
+        "almonds": 25,
+        "rice": 360,
+        "coriander": 1,
+        "lime": 1,
+    },
+    "Chicken and chips": {
+        "chicken": 100,
+        "potatoes": 3,
+        "salt": 1,
+        "malt vinegar": 5,
+    },
+    "Pizza": {
+        "pizza": 1,
+    },
+    "Egg sandwich": {
+        "egg": 2,
+        "bread": 80,
+        "butter": 10,
+    },
+    "Beans on toast": {
+        "beans": 1,
+        "bread": 40,
+    },
+    "Spam a la tin": {
+        "spam": 1,
+        "tin opener": 1,
+        "spoon": 1,
+    },
+}
+
+display_dict = {}
+shopping_list = {}
+shopping_list_key = 1
+
+
+def add_shopping_item(data: dict, item: str, amount: int) -> None:
+    """ This function adds the shopping item into a dict, also sum
+    amount.
+
+    :param data: The dictionary
+    :param item: The item to be bought
+    :param amount: the amount to be bought
+    """
+    data[item] = data.setdefault(item, 0) + amount
+
+
+for index, key in enumerate(recipes):
+    display_dict[str(index + 1)] = key
+
+while True:
+    #  Display a menu of the recipes we know how to cook
+    print("Please, choose your recipe")
+    print("--------------------------")
+    for key, value in display_dict.items():
+        print(f"{key} - {value}")
+
+    choice = input(": ")
+
+    if choice == "0":
+        break
+    elif choice in display_dict:
+        selected_item = display_dict[choice]
+        print(f"You have selected {selected_item}")
+        print("checking ingredients...")
+        ingredients = recipes[selected_item]
+        for food_item, required_quantity in ingredients.items():
+            quantity_in_pantry = pantry.get(food_item, 0)
+            if required_quantity <= quantity_in_pantry:
+                print(f"\t{food_item} OK")
+            else:
+                quantity_to_buy = required_quantity - quantity_in_pantry
+                print(f"\tYou need to buy {quantity_to_buy} of {food_item}")
+                add_shopping_item(shopping_list, food_item, quantity_to_buy)
+
+print("Here is the list of ingredients you should bye for your recipes:")
+print("----------------------------------------------------------------")
+
+for number, (key, value) in enumerate(shopping_list.items()):
+    print(number + 1, (key, value), sep=": ")
